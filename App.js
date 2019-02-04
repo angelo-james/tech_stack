0473750cae87e3e42ec40482db98d79d1eccabf8
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-04 11:50:20 -0800

    imported common components used from past projects and added the header component into app file

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
+import { Header } from './src/components/common'
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View>
-        
+        <Header headerText='Tech Stack' />
       </View>
     </Provider>
   );
