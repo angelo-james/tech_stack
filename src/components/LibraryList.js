03e8486a8b6e70071c7244eaa9d5f56efe4419f7
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-04 13:32:55 -0800

    created a helper funtion that passes props down to list item compoent to render items

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { FlatList } from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library.item} />
+  }
+
   render() {
-    console.log(this.props)
     return (
-      <View>
-        <Text>This is the LibraryList</Text>
-      </View>
+      <FlatList 
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id}
+      />
     )
   }
 }
